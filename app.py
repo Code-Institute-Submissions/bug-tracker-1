3d53195d00f8e39c161abb99f72bd811448548d6
Thiago Hardt <thiago.hardts@gmail.com>
2020-11-15 13:59:15 +0000

    Added cards to accordion
    Tickets description model added

--- app.py
+++ app.py
@@ -19,8 +19,13 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-def index():
-    return render_template("base.html")
+def login():
+    return render_template("login.html")
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
 
 
 if __name__ == "__main__":
