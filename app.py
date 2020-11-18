1a15b947be441e261e457cc5c735d3e56d70b171
Thiago Hardt <thiago.hardts@gmail.com>
2020-11-18 17:21:07 +0000

    Added signup and forgot password templates

--- app.py
+++ app.py
@@ -23,6 +23,16 @@ def login():
     return render_template("login.html")
 
 
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+
+@app.route('/forgot_password')
+def forgot_password():
+    return render_template("forgot_password.html")
+
+
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
--- static/js/script.js
+++ static/js/script.js
@@ -6,4 +6,38 @@ $(document).ready(function () {
     $('.datepicker').datepicker();
     $('select').formSelect();
     $('.carousel').carousel();
+
+    // Confirm Password validation.
+    $("#password").on("focusout", function (e) {
+        if ($(this).val() != $("#confirm-password").val()) {
+            $("#confirm-password").removeClass("valid").addClass("invalid");
+        } else {
+            $("#confirm-password").removeClass("invalid").addClass("valid");
+        }
+    });
+    
+    $("#confirm-password").on("input", function (e) {
+        if ($("#password").val() != $(this).val()) {
+            $(this).removeClass("valid").addClass("invalid");
+        } else {
+            $(this).removeClass("invalid").addClass("valid");
+        }
+    });
+
+    // Confirm Email validation.
+    $("#email").on("focusout", function (e) {
+        if ($(this).val() != $("#confirm-email").val()) {
+            $("#confirm-email").removeClass("valid").addClass("invalid");
+        } else {
+            $("#confirm-email").removeClass("invalid").addClass("valid");
+        }
+    });
+    
+    $("#confirm-email").on("keyup", function (e) {
+        if ($("#email").val() != $(this).val()) {
+            $(this).removeClass("valid").addClass("invalid");
+        } else {
+            $(this).removeClass("invalid").addClass("valid");
+        }
+    });
   });
