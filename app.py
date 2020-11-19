f22703d2fe55d9a03e565c617730bfcde2b36e73
Thiago Hardt <thiago.hardts@gmail.com>
2020-11-19 14:45:11 +0000

    Added login functionality
    Basic authentication added.

--- app.py
+++ app.py
@@ -15,16 +15,63 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+
 mongo = PyMongo(app)
 
 
 @app.route('/')
-def login():
+def index():
     return render_template("login.html")
 
 
-@app.route('/signup')
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Check if username exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Ensure hashed password matches user input
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("dashboard"))
+            else:
+                # Invalid password match
+                flash("Incorrect Username or Password")
+                return redirect(url_for("login"))
+        else:
+            # Username doesn't exist
+            flash("Incorrect Username or Password")
+            return redirect(url_for("login"))
+    return render_template("login.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("signup"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "name": request.form.get("name"),
+            "dob": request.form.get("dob"),
+            "email": request.form.get("email").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("signup.html")
 
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    let chart;
-    const ctx = document.getElementById("chart").getContext("2d");
 
     // UI Initialization
     $(".dropdown-trigger").dropdown();
@@ -10,76 +8,52 @@ $(document).ready(function () {
     $('.datepicker').datepicker();
     $('select').formSelect();
     $('.carousel').carousel();
-    createGraph();
+    // createGraph();
 
-    // Create Graph
-    function createGraph(){
-        var chart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Open', 'In Progress', 'Resolved', 'On Hold'],
-                datasets: [{
-                    label: 'Tickets',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)'
+    // let chart;
+    // const ctx = document.getElementById("chart").getContext("2d");
 
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
+    // // Create Graph
+    // function createGraph(){
+    //     var chart = new Chart(ctx, {
+    //         type: 'bar',
+    //         data: {
+    //             labels: ['Open', 'In Progress', 'Resolved', 'On Hold'],
+    //             datasets: [{
+    //                 label: 'Tickets',
+    //                 data: [12, 19, 3, 5, 2, 3],
+    //                 backgroundColor: [
+    //                     'rgba(255, 99, 132, 0.2)',
+    //                     'rgba(54, 162, 235, 0.2)',
+    //                     'rgba(255, 206, 86, 0.2)',
+    //                     'rgba(75, 192, 192, 0.2)'
+
+    //                 ],
+    //                 borderColor: [
+    //                     'rgba(255, 99, 132, 1)',
+    //                     'rgba(54, 162, 235, 1)',
+    //                     'rgba(255, 206, 86, 1)',
+    //                     'rgba(75, 192, 192, 1)'
+    //                 ],
+    //                 borderWidth: 1
+    //             }]
+    //         },
+    //         options: {
+    //             scales: {
+    //                 yAxes: [{
+    //                     ticks: {
+    //                         beginAtZero: true
+    //                     }
+    //                 }]
+    //             }
+    //         }
+    //     });
+    // }
+    
+    // Toast Fade out
+    $(function() {
+        $('.toast').delay(500).fadeIn('normal', function() {
+           $(this).delay(2500).fadeOut();
         });
-    }
-
-    // Confirm Password validation.
-    $("#password").on("focusout", function (e) {
-        if ($(this).val() != $("#confirm-password").val()) {
-            $("#confirm-password").removeClass("valid").addClass("invalid");
-        } else {
-            $("#confirm-password").removeClass("invalid").addClass("valid");
-        }
-    });
-    
-    $("#confirm-password").on("input", function (e) {
-        if ($("#password").val() != $(this).val()) {
-            $(this).removeClass("valid").addClass("invalid");
-        } else {
-            $(this).removeClass("invalid").addClass("valid");
-        }
-    });
-
-    // Confirm Email validation.
-    $("#email").on("focusout", function (e) {
-        if ($(this).val() != $("#confirm-email").val()) {
-            $("#confirm-email").removeClass("valid").addClass("invalid");
-        } else {
-            $("#confirm-email").removeClass("invalid").addClass("valid");
-        }
-    });
-    
-    $("#confirm-email").on("keyup", function (e) {
-        if ($("#email").val() != $(this).val()) {
-            $(this).removeClass("valid").addClass("invalid");
-        } else {
-            $(this).removeClass("invalid").addClass("valid");
-        }
-    });
-  });
+     });
+});
