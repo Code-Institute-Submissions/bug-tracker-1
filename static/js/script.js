518521f1dc5020e30b45be6567a372c4c1b903b3
Thiago Hardt <thiago.hardts@gmail.com>
2020-11-19 08:54:48 +0000

    Added stats template

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    let chart;
+    const ctx = document.getElementById("chart").getContext("2d");
+
+    // UI Initialization
     $(".dropdown-trigger").dropdown();
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
@@ -6,6 +10,44 @@ $(document).ready(function () {
     $('.datepicker').datepicker();
     $('select').formSelect();
     $('.carousel').carousel();
+    createGraph();
+
+    // Create Graph
+    function createGraph(){
+        var chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Open', 'In Progress', 'Resolved', 'On Hold'],
+                datasets: [{
+                    label: 'Tickets',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)'
+
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
 
     // Confirm Password validation.
     $("#password").on("focusout", function (e) {
