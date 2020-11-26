0bf930413453b07a10e28e3249781d5bd269748e
Thiago Hardt <thiago.hardts@gmail.com>
2020-11-26 13:35:02 +0000

    Added year range for date pickers

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,20 @@
 $(document).ready(function () {
 
+  var date = new Date();
+
     // UI Initialization
     $(".dropdown-trigger").dropdown();
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
     $('.tabs').tabs();
-    $('.datepicker').datepicker();
     $('select').formSelect();
     $('.carousel').carousel();
+    $("#due_date").datepicker({
+      minDate: date
+    });
+    $("#dob").datepicker({
+      yearRange: [1950, 2002]
+    });
 
   // Toast Fade out
   $(function() {
